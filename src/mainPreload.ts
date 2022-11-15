129b99870afed36c0953cdb622d85781eba04210
antieatingactivist <antieatingactivist@gmail.com>
2022-11-15 14:21:13 -0800

    can now upload file and add to torrent list

--- src/mainPreload.ts
+++ src/mainPreload.ts
@@ -1,15 +1,8 @@
 import { contextBridge, ipcRenderer } from 'electron';
-// import * as fs from 'fs';
-
-
-
-
-
-
 
 contextBridge.exposeInMainWorld("api", {
     sendFormData: (formData) => ipcRenderer.send('log-in-attempt', formData),
-    // addFile: (file) => ipcRenderer.once('addFile', file) 
+
 });
 
 ipcRenderer.on("addFile", (e, data) => {
@@ -17,7 +10,7 @@ ipcRenderer.on("addFile", (e, data) => {
   const fileUploadElement: any = document.getElementById('torrent_upload_file');
   openFileButton!.click();
   const dataTransfer = new DataTransfer()
-  const file = new File([data], "test.torrent")
+  const file = new File([data], "torrent.torrent")
   dataTransfer.items.add( file );
   fileUploadElement.files = dataTransfer.files;
   const uploadButton = document.getElementById('upload_confirm_button');
@@ -25,21 +18,4 @@ ipcRenderer.on("addFile", (e, data) => {
 
 })
 
-function test() {
-    // console.log("testLoaded")
-    // const fileUpload: any = document.getElementById('torrent_upload_file');
-    const path = "/Volumes/Garrett/garrett/Downloads/Rick.and.Morty.S06E05.720p.HEVC.x265-MeGusta [IPT].torrent"
 
-    console.log(window)
-    // fs.readFile(path, (err, file) => {
-    //     if (err) {
-    //       console.error(err)
-    //     } else {
-    //       console.log(file)
-         
-    //     }
-    //   })
-    // const fileList: FileList = [file]
-    // fileUpload.value = filename;
-   
-}
