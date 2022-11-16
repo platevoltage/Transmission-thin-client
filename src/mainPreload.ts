8f663ccd9e305dac1b3b5a732fd3053cb4b7669a
antieatingactivist <antieatingactivist@gmail.com>
2022-11-15 16:22:12 -0800

    replaced buttons on interface

--- src/mainPreload.ts
+++ src/mainPreload.ts
@@ -1,8 +1,62 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
+
+setTimeout(() => {
+  const head = document.querySelector('head');
+  const link = document.createElement('link');
+  link.setAttribute('rel', 'stylesheet');
+  link.setAttribute('href', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css')
+  head!.appendChild(link);
+
+
+  const body = document.querySelector('body');
+  const style = document.createElement('style');
+  style.textContent = `
+  * {
+    background-image: none !important;
+  }
+  .torrent {
+    background-color: #333333 !important;
+    border-color: #777777 !important;
+  }
+  .torrent.selected{
+    background-color: #555555 !important;
+  }
+  .torrent_name {
+    color: #ffffff !important;
+  }
+  #toolbar {
+    background-color: #333333 !important;
+    font-size: 34px !important;
+    color: #eeeeee !important;
+  }
+  #toolbar div {
+    margin: 0 !important;
+  }
+  [class^="bi-"]::before, [class*=" bi-"]::before {
+    vertical-align: text-top !important;
+    
+  }`
+  body!.appendChild(style);
+
+
+  function swapIconWithBootstrap(elementName: string, className: string) {
+    const element = document.getElementById(elementName);
+    const icon = document.createElement('i');
+    icon.setAttribute("class", className)
+    element?.appendChild(icon);
+  }
+  swapIconWithBootstrap("toolbar-open", "bi bi-folder2-open");
+  swapIconWithBootstrap("toolbar-remove", "bi bi-x-circle");
+  swapIconWithBootstrap("toolbar-start", "bi bi-play-circle");
+  swapIconWithBootstrap("toolbar-pause", "bi bi-pause-circle");
+  swapIconWithBootstrap("toolbar-start-all", "bi bi-play-circle-fill");
+  swapIconWithBootstrap("toolbar-pause-all", "bi bi-pause-circle-fill");
+},100)
+
+
 contextBridge.exposeInMainWorld("api", {
     sendFormData: (formData) => ipcRenderer.send('log-in-attempt', formData),
-
 });
 
 
